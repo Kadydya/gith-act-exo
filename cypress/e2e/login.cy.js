4a09acddfddb098d50b1aed32cf2636263ad2f83
Kady <kadydya91@yahoo.fr>
2024-04-19 16:39:51 +0200

    creer login

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,11 @@
+describe('tTest suite ghithub', () => {
+  
+  it('should login succefully', () => {
+    cy.visit('https://practice.automationtesteracademy.com/')
+    cy.get('[data-test="username-login"]').type('login_user')
+    cy.get('[data-test="password-login"]').type('cypress-geek')
+    cy.get('[data-test="submit-login"]').click()
+    cy.get('.MuiPaper-root').should('have.length', 8)
+
+  })
+})
